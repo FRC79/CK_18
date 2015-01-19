6c9176dcf74feb2f84bf09392356603289528e90
rspberry-strudel <zachnigels@gmail.com>
2015-01-18 22:21:48 -0500

    Update Robot.java

--- src/krunch18/Robot.java
+++ src/krunch18/Robot.java
@@ -1,50 +1,34 @@
 
-package krunch18;
+package robot;
 
-import krunch18.drivetrain.TeleopDrive;
+import robot.commands.AutonomousCommand;
+import robot.driveTrain.Teleop;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
 	
+	Command autonomousCommand;
+    
     public void robotInit() {
-    	// Load settings and init subsystems
-//    	RobotMap.loadCSVSettings();
     	CommandBase.init();
-    }
-	
-	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-    public void autonomousInit() {
-    }
-
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
-
+    	autonomousCommand = new AutonomousCommand();
+    }   
+    
     public void teleopInit() {
-    	(new TeleopDrive()).start();
-    }
-
-    public void disabledInit(){
-
-    }
-
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
+    	(new Teleop()).start();
     }
     
-    public void testPeriodic() {
-        LiveWindow.run();
+    public void teleopPeriodic() {
+    	Scheduler.getInstance().run();
+    }
+    
+    public void autonomousInit() {
+    	autonomousCommand.start();
+    }
+    
+    public void autonomousPeriodic() {
+    	Scheduler.getInstance().run();
     }
 }
