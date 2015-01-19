fb632e4b3c28a118aee25f268b47d1528736f94d
rspberry-strudel <zachnigels@gmail.com>
2015-01-19 10:07:03 -0500

    Autonomous command group
    groups the three commands move forward, spin, and stop

---
+++ src/krunch18/commands/AutonomousCommand.java
@@ -0,0 +1,34 @@
+
+package robot;
+
+import robot.commands.AutonomousCommand;
+import robot.driveTrain.Teleop;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+
+public class Robot extends IterativeRobot {
+	
+	Command autonomousCommand;
+    
+    public void robotInit() {
+    	CommandBase.init();
+    	autonomousCommand = new AutonomousCommand();
+    }   
+    
+    public void teleopInit() {
+    	(new Teleop()).start();
+    }
+    
+    public void teleopPeriodic() {
+    	Scheduler.getInstance().run();
+    }
+    
+    public void autonomousInit() {
+    	autonomousCommand.start();
+    }
+    
+    public void autonomousPeriodic() {
+    	Scheduler.getInstance().run();
+    }
+}
