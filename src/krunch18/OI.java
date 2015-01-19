cde5bd95bfcd8c7c258d1e5b93316dbe8569ef34
rspberry-strudel <zachnigels@gmail.com>
2015-01-18 22:33:03 -0500

    Update OI.java

--- src/krunch18/OI.java
+++ src/krunch18/OI.java
@@ -1,17 +1,21 @@
-package krunch18;
+package robot;
 
-import edu.wpi.first.wpilibj.Joystick;
+import robot.driveTrain.DriveTrain;
 
-/**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
- */
-public class OI {
-    public Joystick moveStick, rotStick;
-    
-    public OI(){
-    	moveStick = new Joystick(RobotMap.MOVE_STICK_ID);
-    	rotStick = new Joystick(RobotMap.ROT_STICK_ID);
-    }
+import edu.wpi.first.wpilibj.command.Command;
+
+public abstract class CommandBase extends Command {
+	
+	public static OI oi;
+	public static DriveTrain drive;
+
+	protected static void init() {
+		oi = new OI();
+		drive = new DriveTrain();
+	}
+	
+	public CommandBase() {
+		super();
+	}
 }
 
