3838fe346fbbe4498cfb60f68310db9390e31620
Zach <zachnigels@gmail.com>
2015-02-03 16:43:19 -0500

    Command Base

--- src/org/usfirst/frc/team79/robot/CommandBase.java
+++ src/org/usfirst/frc/team79/robot/CommandBase.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team79.robot;
 
-import robot.driveTrain.DriveTrain;
+import org.usfirst.frc.team79.robot.driveTrain.DriveTrain;
+import org.usfirst.frc.team79.subsystems.ToteArm;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -8,13 +9,16 @@ public abstract class CommandBase extends Command {
 	
 	public static OI oi;
 	public static DriveTrain drive;
+	public static ToteArm toteManipulator;
 
 	protected static void init() {
 		oi = new OI();
 		drive = new DriveTrain();
+		toteManipulator = new ToteArm();
 	}
 	
 	public CommandBase() {
 		super();
 	}
 }
+
