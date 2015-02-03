144655f19d016e0a37a5263cc813b61646ad3200
Zach <zachnigels@gmail.com>
2015-02-03 16:45:49 -0500

    Update RobotEnum.java

--- src/org/usfirst/frc/team79/robot/RobotEnum.java
+++ src/org/usfirst/frc/team79/robot/RobotEnum.java
@@ -1,4 +1,4 @@
-package org.usfirst.frc.team79.robot; 
+package org.usfirst.frc.team79.robot;
 
 public enum RobotEnum {
 	
@@ -6,8 +6,10 @@ public enum RobotEnum {
 	BACK_LEFT(1),
 	FRONT_RIGHT(4),
 	BACK_RIGHT(2),
+	LIFT_SYSTEM_MOTOR(5),
+	
 	JOYSTICK_PORT(0),
-	SPIN_BUTTON(1);
+	MANIPULATOR_STICK_ID(3);
 	
 	public final int PORT;
 	
