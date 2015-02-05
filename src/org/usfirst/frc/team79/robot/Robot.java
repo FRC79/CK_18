0fa61a02ab54221606085d043ff3b3acac5ffd73
Zach <zachnigels@gmail.com>
2015-02-05 14:46:38 -0500

    Robot
    Robot go beep boop

--- src/org/usfirst/frc/team79/robot/Robot.java
+++ src/org/usfirst/frc/team79/robot/Robot.java
@@ -1,32 +1,54 @@
 
 package org.usfirst.frc.team79.robot;
 
-import org.usfirst.frc.team79.robot.driveTrain.TeleopArcade;
-import org.usfirst.frc.team79.subsystems.ManualController;
+import org.usfirst.frc.team79.robot.drivetrain.TeleopDrive;
+import org.usfirst.frc.team79.robot.totelift.ManualController;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
-    
+	
+	SendableChooser joystickMode;
+	
     public void robotInit() {
+
     	CommandBase.init();
-    }   
-    
+    	
+    	joystickMode = new SendableChooser();
+    	joystickMode.addDefault("Single Joystick", OI.MODE_SINGLE_JOYSTICK);
+    	joystickMode.addObject("Dual Joysticks", OI.MODE_DUAL_JOYSTICKS);
+    	SmartDashboard.putData("JOYSTICK MODE", joystickMode);
+    }
+	
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+    public void autonomousInit() {
+    }
+
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
     public void teleopInit() {
-    	(new TeleopArcade()).start();
+    	(new TeleopDrive((int)joystickMode.getSelected())).start();
     	(new ManualController()).start();
     }
-    
+
+    public void disabledInit(){
+
+    }
+
     public void teleopPeriodic() {
-    	Scheduler.getInstance().run();
+        Scheduler.getInstance().run();
     }
     
-    public void autonomousInit() {
-    	
-    }
-    
-    public void autonomousPeriodic() {
-    	Scheduler.getInstance().run();
+    public void testPeriodic() {
+        LiveWindow.run();
     }
 }
