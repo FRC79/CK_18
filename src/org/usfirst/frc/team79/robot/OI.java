558c91ab2a601fdd3723382a575a80d20d0747f7
Zach <zachnigels@gmail.com>
2015-02-11 17:51:24 -0500

    OI
    ostiogenesis imperfecta ?

--- src/org/usfirst/frc/team79/robot/OI.java
+++ src/org/usfirst/frc/team79/robot/OI.java
@@ -1,32 +1,25 @@
 package org.usfirst.frc.team79.robot;
 
-import org.usfirst.frc.team79.robot.commands.LiftArm;
-import org.usfirst.frc.team79.robot.commands.LowerArm;
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
-/**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
- */
 public class OI {
     public Joystick moveStick, rotStick;
-    public Joystick liftStick;
-    public JoystickButton lift, lower;
+    public Joystick controlPad;
+    public JoystickButton gripperOpen, gripperClose, gripperArmUp, gripperArmDown, toteLiftUp, toteLiftDown; 
     
     public static int MODE_DUAL_JOYSTICKS = 0;
     public static int MODE_SINGLE_JOYSTICK = 1;
     
     public OI(){
     	moveStick = new Joystick(RobotMap.MOVE_STICK_ID);
-    	liftStick = new Joystick(RobotMap.MANIPULATOR_STICK_ID);
+    	controlPad = new Joystick(RobotMap.MANIPULATOR_STICK_ID);
+
+    	gripperArmDown = new JoystickButton(controlPad, RobotMap.GRIPPER_ARM_DOWN);
+    	gripperArmUp = new JoystickButton(controlPad, RobotMap.GRIPPER_ARM_UP);
     	
-    	lift = new JoystickButton(liftStick, 1);
-    	lower = new JoystickButton(liftStick, 2);
-    	
-    	lift.whenPressed(new LiftArm(0.5, 6.0));
-    	lower.whileActive(new LowerArm(0.5, 6.0));
+    	toteLiftUp = new JoystickButton(controlPad, RobotMap.ELEVATOR_UP);
+    	toteLiftDown = new JoystickButton(controlPad, RobotMap.ELEVATOR_DOWN);
     }
 }
 
