9deee2bea68fde7ac3d34c90be0b55b863cc5ccb
Zach <zachnigels@gmail.com>
2015-02-11 17:48:39 -0500

    RobotMap
    Robot Mappy goodness. Can you find the burried treasure ?

--- src/org/usfirst/frc/team79/robot/RobotMap.java
+++ src/org/usfirst/frc/team79/robot/RobotMap.java
@@ -18,18 +18,40 @@ public class RobotMap {
 	//Lift Motors
 	public static int LIFT_SYSTEM_MOTOR = 4;
 	
+	//Gripper Motors
+	public static int GRIPPER_MOTOR = 6;
+	public static int GRIPPER_ARM_MOTOR = 5;
+	
 	// ANALOG INPUTS
 	public static int GYRO_PORT = 0;
 	
 	// JOYSTICKS
 	public static int MOVE_STICK_ID = 0;
 	public static int ROT_STICK_ID = 1;
-	public static int MANIPULATOR_STICK_ID = 2;
+	public static int MANIPULATOR_STICK_ID;
+	
+	//Gamepad RawAxis
+	public static int LIFT_RAW_VALUE = 3;
+	public static int GRIPPER_ARM_RAW_VALUE = 1;
+	public static int GRIPPER_RAW_VALUE = 0;
 	
 	// Limit Switches
-	public static int TOP_LIMIT_ID = 0;
-	public static int BOTTOM_LIMIT_ID = 1;
-	public static int TOTE_LIMIT_ID = 2;
+	public static int LIFT_TOP_LIMIT_ID = 3;
+	public static int LIFT_BOTTOM_LIMIT_ID = 2;
+	
+	public static int TOTE_ENGGED_ID = 4;
+	
+	public static int GRIP_OPEN_LIMIT_ID = 5;
+	public static int GRIP_CLOSE_LIMIT_ID = 6;
+	
+	public static int ACTUATOR_TOP_ID = 1;
+	public static int ACTUAT0R_BOTTOM_ID = 0;
+	
+	//Controller Buttons
+	public static int ELEVATOR_DOWN = 5;
+	public static int ELEVATOR_UP = 6;
+	public static int GRIPPER_ARM_UP = 2;
+	public static int GRIPPER_ARM_DOWN = 3;
 	
 	// Load configuration settings from CSVs
 	public static void loadCSVSettings(){
