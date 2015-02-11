5bd242e767c1b32aa4a2d789288321d31b5383fb
Zach <zachnigels@gmail.com>
2015-02-11 17:52:35 -0500

    Command Base
    *static* Robot to base, robot to base, we have confirmation, over *static*

--- src/org/usfirst/frc/team79/robot/CommandBase.java
+++ src/org/usfirst/frc/team79/robot/CommandBase.java
@@ -1,28 +1,24 @@
 package org.usfirst.frc.team79.robot;
 
 import org.usfirst.frc.team79.robot.drivetrain.Drivetrain;
-import org.usfirst.frc.team79.robot.totelift.ToteLifter;
+import org.usfirst.frc.team79.robot.subsystems.Gripper;
+import org.usfirst.frc.team79.robot.subsystems.ToteLifter;
+
 
 import edu.wpi.first.wpilibj.command.Command;
-
-/**
- * The base for all commands. All atomic commands should subclass CommandBase.
- * CommandBase stores creates and stores each control system. To access a
- * subsystem elsewhere in your code in your code use CommandBase.exampleSubsystem
- */
 
 public abstract class CommandBase extends Command {
 
 	public static OI oi;
 	public static Drivetrain drivetrain;
-	public static ToteLifter toteManipulator;
+	public static ToteLifter totelifter;
+	public static Gripper binlifter;
 	
 	public static void init(){
-
 		oi = new OI();
+		totelifter = new ToteLifter();
+		binlifter = new Gripper();
 		drivetrain = new Drivetrain();
-		toteManipulator = new ToteLifter();
-		
 	}
 	
 	public CommandBase(String name) {
