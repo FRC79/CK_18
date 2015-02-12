7ac4ba0af3f3db1fb7bed6f29834a16aa388748e
Zach <zachnigels@gmail.com>
2015-02-11 19:45:02 -0500

    Update ConnorsController.java
    It hath been updated..

--- src/org/usfirst/frc/team79/robot/commands/ConnorsController.java
+++ src/org/usfirst/frc/team79/robot/commands/ConnorsController.java
@@ -19,9 +19,9 @@ public class ConnorsController extends CommandBase {
 	protected void execute() {
 		
 		if(oi.toteLiftDown.get()) {
-			oi.toteLiftDown.whileHeld(new ToteLift(-1.0));
+			totelifter.move(-1.0);
 		} else if(oi.toteLiftUp.get()){
-			oi.toteLiftUp.whileHeld(new ToteLift(1.0));
+			totelifter.move(1.0);
 		} else {
 			totelifter.move(oi.moveStick.getRawAxis(RobotMap.LIFT_RAW_VALUE));
 		}
@@ -29,11 +29,6 @@ public class ConnorsController extends CommandBase {
 		binlifter.armPosition(oi.moveStick.getRawAxis(RobotMap.GRIPPER_ARM_RAW_VALUE));
 		binlifter.grip(oi.moveStick.getRawAxis(RobotMap.GRIPPER_RAW_VALUE));
 		
-		//oi.gripperClose.whileHeld(new Gripper(-1.0));
-		//oi.gripperOpen.whileHeld(new Gripper(1.0));
-		
-		//oi.gripperArmDown.whileActive(new MoveGripperArm(-1.0));
-		//oi.gripperArmUp.whileActive(new MoveGripperArm(1.0));
 	}
 
 	@Override
